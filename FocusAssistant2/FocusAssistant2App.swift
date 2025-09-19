09e386685d6c789c94c62e5c9d0a3ba3fb7c5e01
Nana <aaronny13@icloud.com>
2025-09-20 00:20:53 +0100

    Initial Commit

---
+++ FocusAssistant2/FocusAssistant2App.swift
@@ -0,0 +1,32 @@
+//
+//  FocusAssistant2App.swift
+//  FocusAssistant2
+//
+//  Created by Nana Sekyere on 20/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FocusAssistant2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
